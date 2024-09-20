b56877827900d5066f77520489d5e373af80e800
sohumbansal <118093160+sohumbansal@users.noreply.github.com>
2024-09-20 21:32:54 +0530

    Update

--- src/app/env.js
+++ src/app/env.js
@@ -1,9 +1,9 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBt5Ti7YS6TJbXcHjBbhOkxmZFZ7cEIzik",
-  authDomain: "test-d3252.firebaseapp.com",
-  projectId: "test-d3252",
-  storageBucket: "test-d3252.appspot.com",
-  messagingSenderId: "659688032260",
-  appId: "1:659688032260:web:85f845095c8a87f3844d51",
-};
+const  firebaseConfig  = {
+    apiKey:  "Your api key",
+    authDomain:  "your auth domain",
+    projectId:  "your projectId",
+    storageBucket:  "your storage bucket url",
+    messagingSenderId:  "your messaging sender Id",
+    appId:  "Your app Id"
+   };
 export { firebaseConfig };
